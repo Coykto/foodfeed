3bc770e8901b7822a133256262c7dbcbe701a1cf
evgenii.basmov <coykto@gmail.com>
2023-12-04 22:25:25 +0400

    debug memory usage

--- backend/python/lambdas/embedd_and_upload.py
+++ backend/python/lambdas/embedd_and_upload.py
@@ -1,25 +1,39 @@
 import json
 import logging
 
-from dependency.search_client import Search
-from dependency.storage_client import Storage
-from dependency.ai_client import AI
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+def log_memory_usage():
+    import psutil
+    import os
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+    logger.info(f'Memory used: {mem_info.rss / 1024 / 1024} MB')
+
+log_memory_usage()
+
+from dependency.search_client import Search
+from dependency.storage_client import Storage
+from dependency.ai_client import AI
+
+
 def lambda_handler(event, context):
     # init
+    logger.info("++++++++++++++++++++++++")
     venue_slug = event["s"]
     country = "geo"
     city = "tbilisi"
     index_name = f"{country}.{city}.food"
+    log_memory_usage()
 
     # Initialize services
     search = Search()
     storage = Storage()
     ai = AI()
+    log_memory_usage()
 
     # Lambda logic
     if not search.doc_exists(index_name, "venue_slug", venue_slug):
@@ -29,3 +43,5 @@ def lambda_handler(event, context):
         )
         search.bulk(index_name, menu_items)
         return {"s": venue_slug}
+    log_memory_usage()
+    logger.info("------------------------")
