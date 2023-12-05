944ff721b514dcd8ffc3c6c26f58495219c9d2c1
evgenii.basmov <coykto@gmail.com>
2023-12-05 14:29:05 +0400

    almost telegram integration

--- backend/python/lambdas/authorize.py
+++ backend/python/lambdas/authorize.py
@@ -1,8 +1,14 @@
 import json
+import logging
 
 from dependency.config.settings import settings
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def lambda_handler(event, context):
+    logger.info("event", event)
+    logger.info("context", context)
     token = event['headers'].get('X-Telegram-Bot-Api-Secret-Token')
     if not token or token != settings.TELEGRAM_REQUEST_HEADER:
         raise Exception('Unauthorized')
