30faea6abde98b4af291347b62dd18bfa1f856b1
evgenii.basmov <coykto@gmail.com>
2023-12-05 15:03:47 +0400

    almost telegram integration

--- backend/python/lambdas/send_search_result.py
+++ backend/python/lambdas/send_search_result.py
@@ -1,9 +1,14 @@
 import json
-
+import logging
 import requests
 from dependency.config.settings import settings
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def lambda_handler(event, context):
+    logger.info(f"Event: {event}")
+    logger.info(f"Context: {context}")
     item_url = event["url"]
     reason = event["reason"]
     user_id = event["user_id"]
