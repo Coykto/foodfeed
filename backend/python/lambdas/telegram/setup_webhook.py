ea4082563c3a384e9e377478f7f0b140e89dc776
evgenii.basmov <coykto@gmail.com>
2023-12-07 01:47:30 +0400

    separated python stack in smaller stacks

--- backend/python/lambdas/telegram/setup_webhook.py
+++ backend/python/lambdas/telegram/setup_webhook.py
@@ -9,11 +9,11 @@ def lambda_handler(event, context):
     logger.info(f"Event: {event}")
 
     tg_api_url = f"https://api.telegram.org/bot{event['token']}"
-    gateway_url = str(Path(event["webhook_url"]) / "api")
+    gateway_url = f"{event['webhook_url']})api"
     telegram_secret_header = event["secret_header"]
 
     resp = requests.post(
-        Path(tg_api_url) / "setWebhook",
+        f"{tg_api_url}/setWebhook",
         data={
             "url": gateway_url,
             "drop_pending_updates": True,
